d12afd1ab43c52b1bc6356d5684c81a20a35ca7a
Meena-ceutania <81883338+meena-ceutania@users.noreply.github.com>
2021-11-02 13:47:26 -0500

    Update Devtools.js

--- Devtools.js
+++ Devtools.js
@@ -1,3 +1,5 @@
+alert("only can add in Chrome")
+
 (function () { 
   var script = document.createElement('script'); script.scr="//cdn.jsdelivr.net/npm/eruda"; document.body.appendChild(script); script.onload = function () {
     eruda.init() }
